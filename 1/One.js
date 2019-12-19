31eb85d6e635eee90eda2ae12315a3d34145c8c5
Ivan <selentium8923@gmail.com>
2019-12-19 04:23:45 +0300

    Это мой первый комит

---
+++ 1/One.js
@@ -0,0 +1,21 @@
+'use strict'; //es6
+
+var a = 5;
+
+console.log(a);
+
+let Person = {
+    name: "Josh",
+    age: 25,
+    isMarried: false
+};
+
+console.log(Person.name);
+
+let array = ["point.png" , "orange.jpg" , "apple.bmp"];
+
+// let test = +prompt("Are you sure?" , "");
+
+
+console.log(10 * 2 / 4);
+
